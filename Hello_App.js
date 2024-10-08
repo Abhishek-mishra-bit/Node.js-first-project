8715720d5ff5128fc8ee7905784042a01c0df7ee
Abhishek-mishra-bit <abhishekmi1249@gmail.com>
2024-10-08 11:55:55 +0530

    using arrow method and object

--- Hello_App.js
+++ Hello_App.js
@@ -1,2 +1,16 @@
 const fs = require("fs");
 fs.writeFileSync("Hello.txt", "Hello from Node.js");
+
+const addFun = (a, b) => a + b;
+console.log(addFun(3, 4));
+
+const Student = {
+  name: "Abhishek",
+  age: 20,
+  grade: 9,
+  greet() {
+    console.log(`Hii, I am ${Student.name}`);
+  },
+};
+
+Student.greet();
