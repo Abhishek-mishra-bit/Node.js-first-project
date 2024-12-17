8d20ed9c6df68472ef08f0c2a2eb7f436f301af4
Abhishek-mishra-bit <abhishekmi1249@gmail.com>
2024-12-17 12:58:24 +0530

    Dynamic routes part 1

--- models/product.js
+++ models/product.js
@@ -1,35 +1,42 @@
 const fs = require("fs");
 const path = require("path");
-const product = [];
-
 const p = path.join(
   path.dirname(process.mainModule.filename),
   "data",
-  "product.json"
+  "products.json"
 );
-const getProductFromFile = (cb) => {
+const getProductsFromFile = (cb) => {
   fs.readFile(p, (err, fileContent) => {
     if (err) {
-      return cb([]);
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
     }
-    return cb(JSON.parse(fileContent));
   });
 };
-
 module.exports = class Product {
-  constructor(t) {
-    this.title = t;
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
   save() {
-    getProductFromFile((products) => {
+    this.id = Math.random().toString();
+    getProductsFromFile((products) => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log(err);
       });
     });
   }
-
   static fetchAll(cb) {
-    getProductFromFile(cb);
+    getProductsFromFile(cb);
+  }
+  static findById(id, cb) {
+    getProductsFromFile((products) => {
+      const product = products.find((p) => p.id === id);
+      cb(product);
+    });
   }
 };
